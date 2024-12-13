ffae089d2f487182ae3a9e15e43bed7f0d57efcf
Gillgamesh1 <hritikgrd@gmail.com>
2024-12-13 19:05:44 +0530

    2 images will turn to white if selected correctly & tons of new files added

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const cardArray = [
     },
     {
         name : "Ice-Creame",
-        img : "images/ice-creame.png",
+        img : "images/ice-cream.png",
     },
     {
         name : "Milkshake",
@@ -37,7 +37,7 @@ const cardArray = [
     },
     {
         name : "Ice-Creame",
-        img : "images/ice-creame.png",
+        img : "images/ice-cream.png",
     },
     {
         name : "Milkshake",
@@ -52,15 +52,45 @@ const cardArray = [
 console.log(cardArray.sort(()=> 0.5 - Math.random()))
 
 const gridDisplay = document.querySelector('#grid')
+let cardChosenIds = []
+let  cardChosen = []
 
 function createBoard(){
     for (let i = 0; i < cardArray.length; i++){
      
         const card = document.createElement('img')
-        card.setAttribute('scr', 'images/blank.png')
+        card.setAttribute('src', 'images/blank.png')
         card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
         gridDisplay.appendChild(card)
-
+        
     }
 }
 createBoard()
+
+function checkMatch(){
+    
+    const cards = document.querySelectorAll('img')
+    
+    console.log("Check Match working good")
+    let option1 = cardChosen[0]
+    let option2 = cardChosen[1]
+    
+    if (option1 === option2){
+        console.log("This is a match")
+        cards[cardChosenIds[0]].setAttribute('src', 'images/white.png')
+       }else{
+        console.log("Not a match")
+    }
+}
+
+function flipCard() {
+    const cardId = this.getAttribute('data-id')
+    cardChosen.push(cardArray[cardId].name)
+    cardChosenIds.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    
+    if (cardChosen.length == 2){
+        setTimeout(checkMatch, 500)
+    }
+}
